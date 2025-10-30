ace9116971dccddf764035b77b5480598fc9c000
akhilshettyym <akhilshettym2003@gmail.com>
2025-10-30 15:42:34 +0530

    Restaurant dynamic page update

--- app/restaurant/[restaurant].jsx
+++ app/restaurant/[restaurant].jsx
@@ -1,3 +1,213 @@
+// import { View, Text, ScrollView, Platform, FlatList, Dimensions, Image, Linking } from 'react-native'
+// import React, { useEffect, useRef, useState } from 'react'
+// import { useLocalSearchParams } from 'expo-router'
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import { collection, getDocs, query, where } from 'firebase/firestore';
+// import { db } from '../../config/firebaseConfig';
+// import Ionicons from '@expo/vector-icons/Ionicons';
+// import Entypo from '@expo/vector-icons/Entypo';
+// import test from '../../assets/images/landing.jpeg'; // Change req
+// import DatePicker from '../../components/restaurant/DatePicker';
+// import GuestPicker from '../../components/restaurant/GuestPicker';
+// import FindSlots from '../../components/restaurant/FindSlots';
+
+// const Restaurant = () => {
+//     const { restaurant } = useLocalSearchParams();
+
+//     const flatListRef = useRef(null);
+//     const windowWidth = Dimensions.get("window").width;
+//     const [currentIndex, setCurrentIndex] = useState(0);
+
+//     const [date, setDate] = useState(new Date());
+    
+//     const [restaurantData, setRestaurantData] = useState({});
+//     const [carouselData, setCarouselData] = useState({});
+    
+//     const [selectedNumber, setSelectedNumber] = useState(2)
+//     const [slotsData, setSlotsData] = useState({});
+//     const [selectedSlot, setSelectedSlot] = useState(null);
+
+//     const handleNextImage = () => {
+//         const carouselLength = carouselData[0]?.images.length;
+//         if (currentIndex < carouselLength - 1) {
+//             const nextIndex = currentIndex + 1;
+//             setCurrentIndex(nextIndex);
+//             flatListRef.current.scrollToIndex({ index: nextIndex, animated: true });
+//         }
+//         if (currentIndex == carouselLength - 1) {
+//             const nextIndex = 0;
+//             setCurrentIndex(nextIndex);
+//             flatListRef.current.scrollToIndex({ index: nextIndex, animated: true });
+//         }
+//     }
+//     const handlePrevImage = () => {
+//         const carouselLength = carouselData[0]?.images.length;
+//         if (currentIndex > 0) {
+//             const prevIndex = currentIndex - 1;
+//             setCurrentIndex(prevIndex);
+//             flatListRef.current.scrollToIndex({ index: prevIndex, animated: true });
+//         }
+//         if (currentIndex == 0) {
+//             const prevIndex = carouselLength - 1;
+//             setCurrentIndex(prevIndex);
+//             flatListRef.current.scrollToIndex({ index: prevIndex, animated: true });
+//         }
+//     }
+
+//     const carouselItem = ({ item }) => {
+//         return (
+//             <View style={{ width: windowWidth - 2 }} className="h-64 relative rounded-[25px]">
+                
+//                 <View style={{ position: "absolute", top: "40%", backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50, padding: 5, zIndex: 10, right: "6%" }}>
+//                     <Entypo onPress={handleNextImage} name="arrow-with-circle-right" size={30} color="white" />
+//                 </View>
+
+//                 <View style={{ position: "absolute", top: "40%", backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50, padding: 5, zIndex: 10, left: "2%" }}>
+//                     <Entypo onPress={handlePrevImage} name="arrow-with-circle-left" size={30} color="white" />
+//                 </View>
+
+//                 <View style={{ position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row", left: "50%", transform: [{ translateX: -50 }], zIndex: 10, bottom: 15 }}>
+//                     {carouselData[0].images?.map((_, i) => (
+//                         <View key={i} className={`bg-white h-2 w-2 ${i == currentIndex && "h-3 w-3"} p-1 mx-1 rounded-full`} />
+//                     ))}
+//                 </View>
+
+//                 <View>
+//                     <Image source={{ uri: item } || test} style={{ opacity: 0.5, backgroundColor: "black", marginRight: 20, marginLeft: 5, borderRadius: 25 }} className="h-64" />
+//                 </View>
+
+//             </View>
+//         )
+//     }
+
+//     const getRestaurantData = async () => {
+//         try {
+//             const restaurantQuery = query(collection(db, "restaurants"), where("name", "==", restaurant));
+//             const restaurantSnapshot = await getDocs(restaurantQuery);
+
+//             if (restaurantSnapshot.empty) {
+//                 console.log("No matching restaurant found");
+//                 return;
+//             }
+
+//             for (const doc of restaurantSnapshot.docs) {
+//                 const restaurantData = doc.data();
+//                 setRestaurantData(restaurantData);
+
+//                 const carouselQuery = query(collection(db, "carousel"), where("res_id", "==", doc.ref));
+//                 const carouselSnapshot = await getDocs(carouselQuery);
+//                 const carouselImages = [];
+//                 if (carouselSnapshot.empty) {
+//                     console.log("No matching carousel found");
+//                     return;
+//                 }
+//                 carouselSnapshot.forEach((carouselDoc) => {
+//                     carouselImages.push(carouselDoc.data());
+//                 })
+//                 setCarouselData(carouselImages);
+
+//                 const slotsQuery = query(collection(db, "slots"), where("ref_id", "==", doc.ref));
+//                 const slotsSnapshot = await getDocs(slotsQuery);
+//                 const slots = [];
+//                 if (slotsSnapshot.empty) {
+//                     console.log("No matching slots found");
+//                     return;
+//                 }
+//                 slotsSnapshot.forEach((slotDoc) => {
+//                     slots.push(slotDoc.data());
+//                 })
+//                 setSlotsData(slots[0]?.slot);
+
+//             }
+//         } catch (error) {
+//             console.log("Error fetching data", error);
+//         }
+//     }
+
+//     const handleLocation = async () => {
+//         const url = "https://maps.app.goo.gl/TtSmNr394bVp9J8n8";
+//         const supported = await Linking.canOpenURL(url);
+//         if (supported) {
+//             await Linking.openURL(url);
+//         } else {
+//             console.log("Don't know how to open URL", url);
+//         }
+//     }
+
+//     useEffect(() => {
+//         getRestaurantData();
+//     }, []);
+
+//     // console.log( restaurantData, carouselData, slotsData);
+
+//     return (
+//         <SafeAreaView style={[
+//             { backgroundColor: "#000000", flex: 1 },
+//             Platform.OS == "android" && { paddingBottom: 55 },
+//             Platform.OS == "ios" && { paddingBottom: 20 }
+//         ]}>
+//             <ScrollView className="h-full">
+
+//                 <View className="flex-1 my-2 p-2">
+//                     <Text className="text-xl h-10 p-2 mb-2 text-semibold text-[#1ED760]">{restaurant}</Text>
+//                     <View className="border-b border border-[#1ED760]" />
+//                 </View>
+
+//                 <View className="h-64 max-w-[98%] mx-2 rounded-[25px]">
+//                     <FlatList
+//                         ref={flatListRef}
+//                         data={carouselData[0]?.images}
+//                         renderItem={carouselItem}
+//                         horizontal
+//                         scrollEnabled={false}
+//                         showsHorizontalScrollIndicator={false}
+//                         style={{ borderRadius: 25 }}
+//                     />
+//                 </View>
+
+//                 <View className="flex-1 flex-row mt-2 p-2">
+//                     <Ionicons name="location-sharp" size={24} color="#1ED760" />
+//                     <Text className="max-w-[70%] text-white">
+//                         {restaurantData?.address} | {"  "}
+//                         <Text onPress={handleLocation} className="underline flex items-center text-[#1ED760] italic font-semibold mb-2">Get Directions</Text>
+//                     </Text>
+//                 </View>
+
+//                 <View className="flex-1 flex-row p-2">
+//                     <Ionicons name="time" className="mt-1" size={24} color="#1ED760" />
+//                     <Text className="max-w-[70%] mx-2 mt-2 font-semibold text-white">
+//                         {restaurantData?.opening} - {restaurantData?.closing}
+//                     </Text>
+//                 </View>
+
+//                 <View className="border m-2 p-2 border-[#1ED760] rounded-lg">
+//                     <View className="flex-1 flex-row m-2 p-2 justify-end items-center">
+//                         <View className="flex-1 flex-row">
+//                             <Ionicons name="calendar" size={24} color="#1ED760" />
+//                             <Text className="text-white mx-2 text-base"> Select booking date </Text>
+//                         </View>
+//                         <DatePicker date={date} setDate={setDate} />
+//                     </View>
+
+//                     <View className="flex-1 flex-row m-2 bg-[#474747] rounded-lg p-2 justify-end items-center">
+//                         <View className="flex-1 flex-row">
+//                             <Ionicons name="people" size={24} color="#1ED760" />
+//                             <Text className="text-white mx-2 text-base"> Select number of guests </Text>
+//                         </View>
+//                         <GuestPicker selectedNumber={selectedNumber} setSelectedNumber={setSelectedNumber} />
+//                     </View>
+//                 </View>
+                
+//                 <View className="flex-1">
+//                     <FindSlots date={date} selectedNumber={selectedNumber} slots={slotsData} selectedSlot={selectedSlot} setSelectedSlot={setSelectedSlot} />
+//                 </View>
+
+//             </ScrollView>
+//         </SafeAreaView>
+//     )
+// }
+
+// export default Restaurant;
 import { View, Text, ScrollView, Platform, FlatList, Dimensions, Image, Linking } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
@@ -7,7 +217,7 @@ import { db } from '../../config/firebaseConfig';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import Entypo from '@expo/vector-icons/Entypo';
 import test from '../../assets/images/landing.jpeg'; // Change req
-import DatePicker from '../../components/restaurant/datePicker';
+import DatePicker from '../../components/restaurant/DatePicker';
 import GuestPicker from '../../components/restaurant/GuestPicker';
 import FindSlots from '../../components/restaurant/FindSlots';
 
@@ -57,12 +267,15 @@ const Restaurant = () => {
     const carouselItem = ({ item }) => {
         return (
             <View style={{ width: windowWidth - 2 }} className="h-64 relative rounded-[25px]">
+                
                 <View style={{ position: "absolute", top: "40%", backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50, padding: 5, zIndex: 10, right: "6%" }}>
                     <Entypo onPress={handleNextImage} name="arrow-with-circle-right" size={30} color="white" />
                 </View>
+
                 <View style={{ position: "absolute", top: "40%", backgroundColor: "rgba(0,0,0,0.6)", borderRadius: 50, padding: 5, zIndex: 10, left: "2%" }}>
                     <Entypo onPress={handlePrevImage} name="arrow-with-circle-left" size={30} color="white" />
                 </View>
+                
                 <View style={{ position: "absolute", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row", left: "50%", transform: [{ translateX: -50 }], zIndex: 10, bottom: 15 }}>
                     {carouselData[0].images?.map((_, i) => (
                         <View key={i} className={`bg-white h-2 w-2 ${i == currentIndex && "h-3 w-3"} p-1 mx-1 rounded-full`} />
@@ -111,9 +324,9 @@ const Restaurant = () => {
                     return;
                 }
                 slotsSnapshot.forEach((slotDoc) => {
-                    slotsImages.push(slotDoc.data());
+                    slots.push(slotDoc.data());
                 })
-                setSlotsData(slots);
+                setSlotsData(slots[0]?.slot);
 
             }
         } catch (error) {
@@ -145,7 +358,7 @@ const Restaurant = () => {
         ]}>
             <ScrollView className="h-full">
 
-                <View className="flex-1 my-2 p-2">
+                <View className="my-2 p-2">
                     <Text className="text-xl h-10 p-2 mb-2 text-semibold text-[#1ED760]">{restaurant}</Text>
                     <View className="border-b border border-[#1ED760]" />
                 </View>
@@ -162,7 +375,7 @@ const Restaurant = () => {
                     />
                 </View>
 
-                <View className="flex-1 flex-row mt-2 p-2">
+                <View className="flex-row mt-2 p-2">
                     <Ionicons name="location-sharp" size={24} color="#1ED760" />
                     <Text className="max-w-[70%] text-white">
                         {restaurantData?.address} | {"  "}
@@ -170,7 +383,7 @@ const Restaurant = () => {
                     </Text>
                 </View>
 
-                <View className="flex-1 flex-row p-2">
+                <View className="flex-row p-2">
                     <Ionicons name="time" className="mt-1" size={24} color="#1ED760" />
                     <Text className="max-w-[70%] mx-2 mt-2 font-semibold text-white">
                         {restaurantData?.opening} - {restaurantData?.closing}
@@ -178,7 +391,7 @@ const Restaurant = () => {
                 </View>
 
                 <View className="border m-2 p-2 border-[#1ED760] rounded-lg">
-                    <View className="flex-1 flex-row m-2 p-2 justify-end items-center">
+                    <View className="flex-row m-2 p-2 justify-end items-center">
                         <View className="flex-1 flex-row">
                             <Ionicons name="calendar" size={24} color="#1ED760" />
                             <Text className="text-white mx-2 text-base"> Select booking date </Text>
@@ -186,7 +399,7 @@ const Restaurant = () => {
                         <DatePicker date={date} setDate={setDate} />
                     </View>
 
-                    <View className="flex-1 flex-row m-2 bg-[#474747] rounded-lg p-2 justify-end items-center">
+                    <View className="flex-row m-2 bg-[#474747] rounded-lg p-2 justify-end items-center">
                         <View className="flex-1 flex-row">
                             <Ionicons name="people" size={24} color="#1ED760" />
                             <Text className="text-white mx-2 text-base"> Select number of guests </Text>
@@ -195,8 +408,8 @@ const Restaurant = () => {
                     </View>
                 </View>
                 
-                <View className="flex-1">
-                    <FindSlots />
+                <View>
+                    <FindSlots date={date} selectedNumber={selectedNumber} slots={slotsData} selectedSlot={selectedSlot} setSelectedSlot={setSelectedSlot} />
                 </View>
 
             </ScrollView>
--- components/restaurant/FindSlots.jsx
+++ components/restaurant/FindSlots.jsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 
-const FindSlots = () => {
+const FindSlots = ({ date, selectedNumber, slots, selectedSlot, setSelectedSlot }) => {
+  const [slotsVisible, setSlotsVisible] = useState(false);
+
+  const handlePress = () => {
+    setSlotsVisible(!slotsVisible);
+  }
+
+  const handleSlotPress = (slot) => {
+    let prevSlot = selectedSlot;
+    if (prevSlot == slot) {
+      setSelectedSlot(null);
+    } else {
+      setSelectedSlot(slot);
+      // setSlotsVisible(false);
+    }
+  }
+
   return (
-    <View>
-      <Text>FindSlots</Text>
+    <View className="flex-1">
+      <View className={`flex ${selectedSlot != null && "flex-row"}`}>
+
+        <View className={`${selectedSlot != null && "flex-1"}`}>
+          <TouchableOpacity onPress={handlePress}>
+            <Text className="text-center text-white border border-[#1ED760] text-lg font-bold p-2 my-3 mx-2 rounded-lg">Find Slots</Text>
+          </TouchableOpacity>
+        </View>
+
+        {selectedSlot != null && (
+          <View className="flex-1">
+            <TouchableOpacity>
+              <Text className="text-center text-lg font-bold bg-[#1ED760] p-2 my-3 mx-2 rounded-lg">Book Slots</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+      </View>
+
+      {slotsVisible && (
+        <View className="flex-wrap flex-row mx-2 p-2 bg-[#474747] border border-[#1ED760] rounded-lg">
+          {slots.map((slot, index) => (
+            <TouchableOpacity key={index} className={`m-2 p-4 border border-[#1ED760] rounded-lg items-center justify-center ${selectedSlot && selectedSlot !== slot ? "opacity-30" : ""}`} onPress={() => handleSlotPress(slot)} disabled={selectedSlot == slot || selectedSlot == null ? false : true}>
+              <Text className="text-white font-bold">{slot}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
     </View>
   )
 }
 
-export default FindSlots
+export default FindSlots;
